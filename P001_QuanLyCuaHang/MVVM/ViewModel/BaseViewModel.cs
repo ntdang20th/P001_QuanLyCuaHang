8aee90eca1d4de5b8f264a4eb76fdead20f75325
ntdang_20th@student.agu.edu.vn <ntdang_20th@student.agu.edu.vn>
2022-06-22 08:03:35 +0700

    Demo github commit

---
+++ P001_QuanLyCuaHang/MVVM/ViewModel/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P001_QuanLyCuaHang.MVVM.ViewModel
+{
+    public class BaseViewModel
+    {
+    }
+}
