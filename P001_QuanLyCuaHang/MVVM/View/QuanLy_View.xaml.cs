58e1433e71947e0950d9bcf32c4da1c55c948fd3
nvky_20th@student.agu.edu.vn <nvky_20th@student.agu.edu.vn>
2022-06-22 16:20:56 +0700

    commit đi ăn cơm

---
+++ P001_QuanLyCuaHang/MVVM/View/QuanLy_View.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P001_QuanLyCuaHang.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for QuanLy_View.xaml
+    /// </summary>
+    public partial class QuanLy_View : UserControl
+    {
+        public QuanLy_View()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            switch (index)
+            {
+                case 0:
+                    {
+                        GridSmallMain.Children.Clear();
+                        GridSmallMain.Children.Add(new MVVM.View.HangHoa_View());
+                        break;
+                    }
+                case 1:
+                    {
+                        GridSmallMain.Children.Clear();
+                        GridSmallMain.Children.Add(new MVVM.View.HoaDonNhapView());
+                        break;
+                    }
+                case 2:
+                    {
+                        
+                        break;
+                    }
+                case 3:
+                    {
+                        
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+            }
+        }
+
+    }
+}
--- P001_QuanLyCuaHang/MainWindow.xaml.cs
+++ P001_QuanLyCuaHang/MainWindow.xaml.cs
@@ -20,9 +20,73 @@ namespace P001_QuanLyCuaHang
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool IsMaximized = false;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximized = true;
+                }
+            }
+        }
+
+        private void Button_menu_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            switch (index)
+            {
+                case 0:
+                    {
+                        GridMain.Children.Clear();
+                        GridMain.Children.Add(new MVVM.View.QuanLy_View());
+                        break;
+                    }
+                case 1:
+                    {
+                        break;
+                    }
+                case 2:
+                    {
+                        GridMain.Children.Clear();
+                        GridMain.Children.Add(new MVVM.View.KhachHang_View());
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+            }
+        }
+
     }
 }
